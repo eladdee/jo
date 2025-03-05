be4282c65f7581cb4279d955be0a4fe663da44f1
ddd <eladevrim65@gmail.com>
2025-03-05 14:38:05 +0300

    agd
    fgadfg

---
+++ view/deposit.php
@@ -0,0 +1,3 @@
+<section class="SectionContent ">
+   <div class="HostedCashierDepositIframeContainer"><iframe title="Hosted Cashier Deposit Iframe" src="/payment/" class="HostedCashierDepositIframe" style="height: 952px;"></iframe></div>
+</section>
