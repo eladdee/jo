24a89ba347e63482c9bd2c2cae96da15a2f9a97a
ddd <eladevrim65@gmail.com>
2025-03-08 12:09:29 +0300

    gadf
    gadf

--- view/promotions.php
+++ view/promotions.php
@@ -1,7 +1,7 @@
 <section class="SectionContent">
    <div class="OperatorSlider Slider SliderSwipe">
       <section class="slider" aria-label="carousel-slider" role="region" tabindex="0" style="box-sizing: border-box; display: block; height: inherit; position: relative; width: 100%;">
-         <div class="slider-frame" style="box-sizing: border-box; display: block; height: 25%; margin: 0px; overflow: hidden; padding: 0px; position: relative; touch-action: pan-y pinch-zoom; transform: translate3d(0px, 0px, 0px);">
+         <div class="slider-frame" style="box-sizing: border-box; display: block; height: 44%; margin: 0px; overflow: hidden; padding: 0px; position: relative; touch-action: pan-y pinch-zoom; transform: translate3d(0px, 0px, 0px);">
             <div class="slider-list" style="box-sizing: border-box; cursor: pointer; display: block; height: 330px; margin: 0px; padding: 0px; position: relative; touch-action: pan-y pinch-zoom; transform: translate3d(0px, 0px, 0px); width: auto; transition: all;">
                <div class="slider-slide" aria-label="slide 1 of 10" role="group" tabindex="-1" style="box-sizing: border-box; display: inline-block; height: 330px; left: 0px; list-style-type: none; margin: auto 0px; position: absolute; top: 0px; transform: scale(1); transition: transform 0.4s linear; vertical-align: top; width: 1389px;" inert="true">
                   <div class="SliderItem">
