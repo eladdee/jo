d1c53ffc5fdbf464cd52ebca61cb3ccbb3b0d780
ddd <eladevrim65@gmail.com>
2025-03-15 18:21:12 +0300

    adgad
    fgadf

--- view_mob/deposit.php
+++ view_mob/deposit.php
@@ -1,3 +1,5 @@
-<section class="SectionContent ">
+<section class="SectionContent "style="
+    background: #e5e6e7;
+">
    <div class="HostedCashierDepositIframeContainer"><iframe title="Hosted Cashier Deposit Iframe" src="/payment/" class="HostedCashierDepositIframe" style="height: 952px;"></iframe></div>
 </section>
